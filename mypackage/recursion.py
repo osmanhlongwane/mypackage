5844b11180ef20e08da04b49034aa143b7b8099f
osmanhlongwane <40489174+osmanhlongwane@users.noreply.github.com>
2019-03-19 17:39:41 +0200

    Update recursion.py

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -19,13 +19,6 @@ def sum_array(array):
     else:
         return array[0]+ sum_array(array[1:])
 
-def fibonacci(n):
-    
-    '''Return nth term in fibonacci sequence'''
-    if n <= 1:
-        return n
-    else:
-        return(recur_fibo(n-1) + recur_fibo(n-2))
 
 def factorial(n):
     
@@ -36,6 +29,14 @@ def factorial(n):
         lower_fact = factorial(n-1)
         current_fact = n * lower_fact
         return  current_fact
+       
+def fibonacci(n):
+ '''Return nth term in fibonacci sequence'''
+ if n <= 1:
+  return n
+ else:
+  return(recur_fibo(n-1) + recur_fibo(n-2))
+
 
 
 def reverse(word):
